b800232b26a079896a1175c5904d4f41afec36ea
officialsanchi <nwangwudinma22@gmail.com>
2025-03-03 18:44:40 +0100

    validate

--- schoolapp/src/schoolapp/cource.py
+++ schoolapp/src/schoolapp/cource.py
@@ -1,4 +1,38 @@
+from schoolapp.src.schoolapp import instructor
 
+
+class Course :
+    def _init_(self, course_name, course_code,course_grade):
+        self._course_name = course_name
+        self._course_code = course_code
+        self._course_grade = course_grade
+        self._instructor = instructor.Instructor("name", "password@gmail.com", "password", "T01","English language")
+
+
+    @property
+    def course_name(self):
+        return self._course_name
+    @property
+    def course_code(self):
+        return self._course_code
+
+    @course_name.setter
+    def course_name(self, course_name):
+        self._course_name = course_name
+
+    @course_code.setter
+    def course_code(self, course_code):
+        self._course_code = course_code
+
+    @property
+    def course_grade(self):
+        return self._course_grade
+    @course_grade.setter
+    def course_grade(self, course_grade):
+        self._course_grade = course_grade
+
+    def get_instructor(self):
+        return self._instructor.name
 
 
 
