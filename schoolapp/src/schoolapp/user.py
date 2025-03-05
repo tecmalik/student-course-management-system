c5bc35f7092bdb879acdc65b974adb2bb798c279
tecmalik <abdulmalikojo2@gmail.com>
2025-03-05 22:05:25 +0100

    updating files

--- schoolapp/src/schoolapp/user.py
+++ schoolapp/src/schoolapp/user.py
@@ -15,9 +15,8 @@ def validate_email(email):
         return email
     else:
         raise Exception("Invalid email")
-
-
-
+pattern_password = r"^(?=.[A-Z])(?=.[a-z])(?=.\d)(?=.[!@#$%^&])[A-Za-z\d!@#$%^&]{8,}$"
+email_pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
 class User(ABC):
     def __init__(self, name: str ,email: str, password: str ):
         self._name = name
@@ -30,8 +29,18 @@ class User(ABC):
 
     @abstractmethod
     def login(self, email, password):
-        pass
+        return self.email == email and self.password == password
 
     @name.setter
     def name(self, value):
         self._name = value
+
+    def register(self, email, password, name):
+        if not re.match(email_pattern, email):
+            raise ValueError("Invalid email format.")
+        if not re.match(pattern_password, password):
+            raise ValueError("Password must be at least 8 characters with one letter and one number.")
+        self.email = email
+        self.password = password
+        self.name = name
+        return True
