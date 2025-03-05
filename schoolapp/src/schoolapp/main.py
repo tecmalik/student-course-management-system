1673a63f40eb4a0983e54f4cf8322900a981c975
tecmalik <abdulmalikojo2@gmail.com>
2025-03-05 03:01:09 +0100

    updating files

---
+++ schoolapp/src/schoolapp/main.py
@@ -0,0 +1,35 @@
+
+prompt = """
+    Welcome
+    1. login
+    2.register
+"""
+login_prompt= """
+    1.login as An instructor
+    2. login as A student
+"""
+registration_prompt ="""
+1. register as an instructor
+2. register as a Student
+"""
+
+def userInput(prompt):
+    return input(prompt)
+
+
+def registration_menu():
+    return input(registration_prompt)
+
+def login_menu():
+    return input(login_prompt)
+
+
+def first_menu():
+    match userInput(prompt):
+        case '1': login_menu()
+        case '2': registration_menu()
+        case _ :
+            print('invalid input')
+            first_menu()
+
+    
